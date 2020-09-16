98c298d1afcaa2885b63442b00cd95b194c0b3b9
mlowcher <mlowcher@redhat.com>
2020-09-16 08:22:32 -0700

    ping to -c and shell true

--- ip_reach.py
+++ ip_reach.py
@@ -7,7 +7,7 @@ def ip_reach(iplist):
     for ip in iplist:
         ip = ip.rstrip("\n")
         
-        ping_reply = subprocess.call('ping %s /n 2' % (ip), stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+        ping_reply = subprocess.call('ping %s -c 2' % (ip), stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL, shell=True)
                
         if ping_reply == 0:
             print("\n* {} is reachable :)\n".format(ip))
